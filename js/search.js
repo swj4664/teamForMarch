46a7c41fb4d5216ca6194ad31de9d2d63adc2d29
swj4664 <108079743+swj4664@users.noreply.github.com>
2023-04-02 01:22:20 +0900

    Add files via upload

--- js/search.js
+++ js/search.js
@@ -4,9 +4,9 @@ let inputSearch = document.querySelector('.left input')
 let smallImg = document.querySelectorAll('.smallImg img')
 let wrapWeather = document.querySelector('.wrapWeather')
 
-search.addEventListener('click', () => {
-    wrapWeather.classList.add('on')
-})
+// search.addEventListener('click', () => {
+//     wrapWeather.classList.add('on')
+// })
 
 cancel.addEventListener('click', () => {
     wrapWeather.classList.remove('on')
@@ -15,15 +15,15 @@ cancel.addEventListener('click', () => {
 
 smallImg[0].style.border = '4px solid #5707ff'
 smallImg[0].style.opacity = '100%'
-for(let i in smallImg){
+for (let i in smallImg) {
     imgClick(i)
 }
 
-function imgClick(i){
-    smallImg[i].addEventListener('click', function() {
-        smallImg.forEach((e)=>(e.style.border = 'none', e.style.opacity = '50%'))
+function imgClick(i) {
+    smallImg[i].addEventListener('click', function () {
+        smallImg.forEach((e) => (e.style.border = 'none', e.style.opacity = '50%'))
         smallImg[i].style.border = '4px solid #5707ff',
-        smallImg[i].style.opacity = '100%'
+            smallImg[i].style.opacity = '100%'
         i++
         let bImg = ''
         bImg += `<img src="./img/seoul${i}.png" alt="" />`
@@ -32,9 +32,13 @@ function imgClick(i){
     })
 }
 
-function searchTravel(){
+function searchTravel() {
     let travelSearch = document.getElementById('travelSearch').value
-    $('.title h2').html(travelSearch)
-    wrapWeather.classList.add('on')
+    if (travelSearch !== "") {
+        $('.title h2').html(travelSearch)
+        wrapWeather.classList.add('on')
+    } else {
+        alert('여행상품을 입력해주세요.')
+    }
 }
 
